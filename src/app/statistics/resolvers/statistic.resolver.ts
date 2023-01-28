ba3791a2bd6a559bd120d610503b9b1e28f70905
Lesha <lesha@MacBook-Pro-Lesha.local>
2023-01-28 12:09:36 +0100

    stat guard

--- src/app/statistics/resolvers/statistic.resolver.ts
+++ src/app/statistics/resolvers/statistic.resolver.ts
@@ -1,5 +1,9 @@
+import { UseGuards } from "@nestjs/common";
 import { Args, Query, Resolver } from "@nestjs/graphql";
 
+import { GqlJwtGuard } from "../../auth";
+import { RolesGuard } from "../../shared";
+import { UserRoleEnum } from "../../shared/enums";
 import { StatisticType } from "../dto/statistic.dto";
 import { StatisticService } from "../services";
 
@@ -8,6 +12,7 @@ export class StatisticResolver {
 	constructor(private readonly _statisticService: StatisticService) {}
 
 	@Query(() => StatisticType)
+	@UseGuards(GqlJwtGuard, RolesGuard([UserRoleEnum.ADMIN, UserRoleEnum.MANAGER]))
 	async getPlaceStatistic(@Args("placeId") placeId: string) {
 		return this._statisticService.calculateStatistic(placeId);
 	}
