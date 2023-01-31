4bbdc0d4f39702004cc74dbfc67fec51e8ae3d85
Viacheslav Saloid <viacheslavsaloid.work@gmail.com>
2023-02-01 01:35:49 +0200

    Feat: serve static

--- src/app/core/configs/server-static.config.ts
+++ src/app/core/configs/server-static.config.ts
@@ -1,5 +1,7 @@
+import type { ServeStaticModuleOptions } from "@nestjs/serve-static";
 import { join } from "path";
 
-export const SERVER_STATIC_CONFIG = {
-	rootPath: join(__dirname, "./assets")
+export const SERVER_STATIC_CONFIG: ServeStaticModuleOptions = {
+	rootPath: join(__dirname, "./assets"),
+	serveRoot: "/assets"
 };
