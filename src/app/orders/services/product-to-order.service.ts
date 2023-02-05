fd3f7ac2b0cdc26b5ae01212eb7f1324e36be1e3
Lesha <lesha@lan-102.efx.ro>
2023-02-05 21:08:40 +0200

    fix

--- src/app/orders/services/product-to-order.service.ts
+++ src/app/orders/services/product-to-order.service.ts
@@ -165,7 +165,7 @@ export class ProductToOrderService {
 			where: {
 				id: In(productToOrderIds)
 			},
-			relations: ["order", "order.users"]
+			relations: ["order", "order.table", "product", "order.users"]
 		});
 
 		const updatedPtos = pTos.map((el) => ({ ...el, status: ProductToOrderStatusEnum.REJECTED }));
@@ -178,7 +178,7 @@ export class ProductToOrderService {
 			where: {
 				id: In(productToOrderIds)
 			},
-			relations: ["order", "order.users"]
+			relations: ["order", "order.table", "product", "order.users"]
 		});
 
 		const updatedPtos = pTos.map((el) => ({ ...el, status: ProductToOrderStatusEnum.APPROVED }));
@@ -191,7 +191,7 @@ export class ProductToOrderService {
 			where: {
 				id: In(productToOrderIds)
 			},
-			relations: ["order", "order.users"]
+			relations: ["order", "order.table", "product", "order.users"]
 		});
 
 		const updatedPtos = pTos.map((el) => ({ ...el, paidStatus: ProductToOrderPaidStatusEnum.WAITING }));
@@ -204,7 +204,7 @@ export class ProductToOrderService {
 			where: {
 				id: In(productToOrderIds)
 			},
-			relations: ["order", "order.users"]
+			relations: ["order", "order.table", "product", "order.users"]
 		});
 
 		const updatedPtos = pTos.map((el) => ({ ...el, paidStatus: ProductToOrderPaidStatusEnum.PAID }));
