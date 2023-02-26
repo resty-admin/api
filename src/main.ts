3cc39763048ec9616877001ac9e6d6cc16e50ddb
Lesha <lesha@MacBook-Pro-Lesha.local>
2023-02-26 23:36:30 +0200

    fix

--- src/main.ts
+++ src/main.ts
@@ -13,8 +13,10 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.useGlobalPipes(
 		new ValidationPipe({
-			transform: true
-			// , whitelist: true
+			disableErrorMessages: false,
+			transform: true,
+			forbidNonWhitelisted: false,
+			whitelist: false
 		})
 	);
 
